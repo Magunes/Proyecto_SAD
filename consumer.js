76be7f8b8528759b225396d008c3973cfb911f12
Álvaro <alvarolebval@gmail.com>
2023-01-16 22:38:04 +0100

    Worker elimina directorio & opcion de frontend para recuperar el resultado de un trabajo

--- consumer.js
+++ consumer.js
@@ -2,33 +2,53 @@ const kafka = require('./kafka')
 const download = require('download-git-repo')
 const {spawn} = require('child_process')
 const consumer = kafka.consumer({
-   groupId: "group-id"
+   groupId: "workers"
 })
+const producer = kafka.producer()
+const fs = require('fs')
+
 
 const main = async () => {
+   await producer.connect()
    await consumer.connect()
    await consumer.subscribe({
-      topic: "topic_prueba",
+      topic: "Entrada",
       fromBeginning: true
    })
 
    await consumer.run({
       eachMessage: async ({ message }) => {
+
 	 const addr = message.value.toString().slice(1,-1)
-	 //const date = Date.now() ESTO SI NO SE USA UUID EN EL SERVER
-         /*download('direct:'+addr+'.git','test/',{ clone:true },function(err){
-            console.log(err ? 'Exito al clonar' : 'Error al clonar')
-         })*/ //EL GIT DE GONZALO NO FUNCIONA EXACTAMENTE
-         var process = spawn('python',["./test/index.py"])
-         process.stdout.on('data', function(data){
-            console.log(data.toString())
-         })
-	 process.on('close', (code) => {
-	    console.log('Trabajo Finalizado con codigo ${code}')
-	 })
-	 //Pasar ahora a cola kafka con resultados usando de key el uuid
-	 //Consumir de la cola kafka el ID del usuario y ver los trabajos que ha enviado
-	      //añadiendo el trabajo actual, esto es posible si existe retain
+	 const topic = message.key.toString()
+	 var control = 1 //asociar el valor de control según el resultado de download
+
+         download('direct:'+addr+'.git','test/',{ clone:true },function(err){
+	     console.log(err ? "1" : "2")
+	 }) 
+
+         if(control){
+             console.log("Clonado correcto: "+addr)
+             var process = spawn('python',["./test/index.py"])
+
+             process.stdout.on('data',async function(data){
+		 console.log(data.toString())
+                 await producer.send({
+                     topic: topic,
+                     messages: [ { value: data.toString() } ]
+                 })
+		 //El directorio se borra por alguna razon antes o relacionado con la creacion del directorio
+		 //fs.rmSync("./test/", {recursive: true, force: true})
+             })
+
+	 } else{
+             console.log("Error clonado")
+	     producer.send({
+	         topic: topic,
+		 messages: [ { value: "El repositorio no ha podido clonarse, asegurate de que es un repositorio publico" } ]
+	     })
+	 }
+        
       }
    })
 }
--- server.js
+++ server.js
@@ -1,36 +1,78 @@
 const kafka = require('./kafka')    
 const express = require('express')
-const { v4: uuidv4 } = require('uuid');
-
-var app = express()    
+const { v4: uuidv4 } = require('uuid')
+const consumer = kafka.consumer({
+    groupId: "servers"
+})
 const producer = kafka.producer()      
+
+var app = express()
 app.use(express.urlencoded())
 
 const main = async () => {
-   await producer.connect()  
+   await producer.connect()
 
    app.get('/enviar_trabajo', function(req,res,next) {
-      res.send(`<form method= "POST" action="/">
+      res.send(`
+      <form method= "POST" action="/">
       <input type="text" name="url" placeholder="Aquí tu url :)">
       <input type="submit">
-      </form>`)
+      </form>
+      <br>
+      <form method= "POST" action="/get_trabajo">
+      <input type="text" name="codigo" placeholder="Numero de trabajo">
+      <input type="submit">
+      </form>
+      `)
    })
 
    app.post('/', function(req,res,next){
-      var url = JSON.stringify(req.body.url)
+
+      const url = JSON.stringify(req.body.url)
+      const date = Date.now().toString()
+
       producer.send({
-         topic: "topic_prueba",
-         messages: [ { key: uuidv4(),value: url } ]
+         topic: "Entrada",
+         messages: [ { key: date, value: url } ]
       })
-      res.send('OK')
+
+      res.send('Trabajo enviado, el codigo de trabajo es: '+date)
    })
+
+   app.post('/get_trabajo', async function(req,res,next){
+
+       var code = JSON.stringify(req.body.codigo)
+       code = code.toString().slice(1,-1)
+
+       await consumer.connect()
+
+       await consumer.subscribe({
+	   topic: code,
+	   fromBeginning: true
+       })
+
+       await consumer.run({
+	   eachMessage: async ({ message }) => {
+	       const result = message.value.toString()
+	       res.send('El resultado del trabajo es: <br>'+ result)
+	       }
+       })
+
+       //Es necesario desconectar al consumer. antes de que pase a otro codigo
+
+    })
 
    app.listen(3000, () => {
       console.log('Escuchando')
    })
 }
 
-main().catch(error => {
+main().catch(async error => {
    console.error(error)
+   try {
+      await consumer.disconnect()
+   } catch (e) {
+      console.error('Failed to gracefully disconnect consumer', e)
+   }
    process.exit(1)
 })
