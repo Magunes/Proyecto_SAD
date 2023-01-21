11a55a3d6230fb258543da7a9ef4af2203eb63e3
Álvaro <alvarolebval@gmail.com>
2023-01-21 20:25:46 +0100

    Funcionalidades acabadas para el servidor sin keycloak && Formateo de html

--- server.js
+++ server.js
@@ -26,24 +26,35 @@ const main = async () => {
    await consumer.run({
       eachMessage: async ({ message }) => {
          const result = message.value.toString()
-         //res.send('El resultado del trabajo es: <br>'+ result)
+         const auth = message.key.toString().slice(0,10)
+
+         jobsResult.set(auth,result)
       }
    })
 
    app.get('/index', function(req,res,next) {
       res.send(`
+      <h2>Sistema de procesado de trabajos</h2>
+
       <form method= "POST" action="/enviar_trabajo">
-      <input type="text" name="url" placeholder="URL de git">
-      <input type="submit">
+         <label>Enviar trabajo</label><br>
+         <input type="text" name="url" placeholder="URL del repositorio GIT">
+         <input type="submit" value="Enviar Trabajo">
       </form>
+
       <br>
+
       <form method= "POST" action="/estado_trabajo">
-      <input type="text" name="codigo" placeholder="Numero de trabajo">
-      <input type="submit">
+         <label>Comprobar estado de trabajo</label><br>
+         <input type="text" name="codigo" placeholder="Codigo de trabajo">
+         <input type="submit" value="Comprobar trabajo">
       </form>
+
       <br>
+
       <form method= "POST" action="/lista_trabajo">
-      <input type="submit">
+         <label>Listado de trabajos enviados</label><br>
+         <input type="submit" value="Comprobar listado">
       </form>
       `)
    })
@@ -72,11 +83,45 @@ const main = async () => {
          +'<br>Con URL: '+req.body.url+
          `<br><br>
          <form method= "GET" action="/index">
-         <input type="submit">
+         <input type="submit" value="Volver">
          </form>`)
    })
 
    app.post('/estado_trabajo', function(req,res,next){
+      if(jobs.has(user)){
+         var temp = jobs.get(user)
+         if(temp.indexOf(req.body.codigo)+1 != 0){
+            if(jobsResult.has(req.body.codigo)){
+               var toret = "El resultado del trabajo con codigo: "+req.body.codigo+" es: <br><br>"+jobsResult.get(req.body.codigo)+
+                  `<br><br>
+                  <form method= "GET" action="/index">
+                  <input type="submit" value="Volver">
+                  </form>`
+               res.send(toret)
+            }
+            else{
+               res.send(`El trabajo con codigo: `+req.body.codigo+`, está siendo procesado 
+               <br><br>
+               <form method= "GET" action="/index">
+               <input type="submit" value="Volver">
+               </form>`)
+            }
+         }
+         else{
+            res.send(`El usuario actual (`+user+`) no tiene trabajos con el codigo: `+req.body.codigo+` 
+               <br><br>
+               <form method= "GET" action="/index">
+               <input type="submit" value="Volver">
+               </form>`)
+         }
+      }
+      else{
+         res.send(`El usuario actual (`+user+`) no ha enviado trabajos
+            <br><br>
+            <form method= "GET" action="/index">
+            <input type="submit" value="Volver">
+            </form>`)
+      }
    })
 
    app.post('/lista_trabajo', function(req,res,next){
@@ -86,13 +131,17 @@ const main = async () => {
          temp.forEach(function(value){
             toret += "  -"+value+"<br><br>"
          })
+         toret +=
+         `<form method= "GET" action="/index">
+         <input type="submit" value="Volver">
+         </form>`
          res.send(toret)
       }
       else{
          res.send(`El usuario actual (`+user+`) no ha enviado trabajos
             <br><br>
             <form method= "GET" action="/index">
-            <input type="submit">
+            <input type="submit" value="Volver">
             </form>`)
       }
    })
